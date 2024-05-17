1c1dad21ef40a6a814ab0f4c61125d5aba280a80
yogipatel1461 <yogkp49@gmail.com>
2024-05-17 14:52:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('chatbot-bubble').addEventListener('click', function() {
+    // Code to open chatbot interface goes here
+    alert('Chatbot interface should open now.');
+});
